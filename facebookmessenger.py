a9734451abff7e5446a4ead802ad9a85b0809965
HeliosLHC <groudon2224@gmail.com>
2017-05-27 22:02:24 -0400

    Final Commit

--- facebookmessenger.py
+++ facebookmessenger.py
@@ -1,21 +1,30 @@
 import fbchat
+import requests
+import json
+
+def  getName(author_id):
+	url = "https://graph.facebook.com/" + author_id + "?fields=name&access_token=1910943402516729|NFxHu_FCpNW-Cj1zHRIIfoB5FQI"
+	FBresponse = requests.get(url)
+	parsed_json = json.loads(FBresponse.text)
+	print parsed_json["name"]
+	return parsed_json["name"]
 # # Logon to Facebook
-client = fbchat.Client("moodybotchat@gmail.com", "moodybotchatstuyhacks")
+# client = fbchat.Client("moodybotchat@gmail.com", "moodybotchatstuyhacks")
 # # Message
 # # Retrieve Friends List
 
-
-def setContact():
-	global friend
-	global friends
-	friends = client.getUsers(contactName)
-	# print filter(lambda x: 'Matthew Cen' in x, friends)
-	# indices = [i for i, s in enumerate(friends) if contactName in s]
-	# print indices
-	# friendListIndex = friends.index(contactName)
-	print friends
-	# Select Friend to Talk To
-	friend = friends[0]	
+#
+# def setContact():
+# 	global friend
+# 	global friends
+# 	friends = client.getUsers(contactName)
+# 	# print filter(lambda x: 'Matthew Cen' in x, friends)
+# 	# indices = [i for i, s in enumerate(friends) if contactName in s]
+# 	# print indices
+# 	# friendListIndex = friends.index(contactName)
+# 	print friends
+# 	# Select Friend to Talk To
+# 	friend = friends[0]	
 
 # # Send User Response
 # messageSend = "Hello I'm moody bot"
@@ -42,9 +51,11 @@ class EchoBot(fbchat.Client):
     def on_message(self, mid, author_id, author_name, message, metadata):
         self.markAsDelivered(author_id, mid) #mark delivered
         self.markAsRead(author_id) #mark read
+        
+        moodyResponse = BotResponse(getName(author_id))
         # if you are not the author, echo
         if str(author_id) != str(self.uid):
-            self.send(author_id,author_id)
+            self.send(author_id,moodyResponse)
 
 bot = EchoBot("moodybotchat@gmail.com", "moodybotchatstuyhacks")
 bot.listen()
